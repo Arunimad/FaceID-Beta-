8d6f9c9a5a914df40d7f59cd477cc5f5f96ab8be
Arunimad <rarunrd@gmail.com>
2018-02-05 12:27:45 +0530

    Not-Optimised Code

---
+++ FaceID.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace FaceID_Protocol
+{
+    partial class FaceID
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(137, 108);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1062, 666);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(488, 780);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Webcam";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(488, 818);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1657, 808);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 32);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Add Face";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1495, 818);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1478, 709);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(152, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1429, 610);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(251, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(326, 63);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(540, 22);
+            this.dateTimePicker2.TabIndex = 7;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1292, 247);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(493, 283);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // FaceID
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1777, 929);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FaceID";
+            this.Text = "FaceID";
+            this.Load += new System.EventHandler(this.FaceID_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ FaceID.cs
@@ -0,0 +1,527 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Accord.Video;
+using Accord.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.Face;
+using Emgu.CV.Structure;
+using MetroFramework.Forms;
+using Tulpep.NotificationWindow;
+
+namespace FaceID_Protocol
+{
+
+
+
+
+    public partial class FaceID : MetroForm
+
+    {
+
+
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private CascadeClassifier haarcascade_frontalface_default;
+
+
+        Image<Gray, Byte> gray_face = null;
+
+        private int k = 0;
+        
+
+
+        List<Image<Gray, Byte>> trainingImages = new List<Image<Gray, Byte>>();
+        List<int> labels = new List<int>();
+        List<string> NamePersons = new List<string>();
+
+
+
+        Image<Gray, Byte>[] face = new Image<Gray, Byte>[50];
+        String[] faceName = new String[50];
+        int[] faceNum = new int[50];
+
+
+
+        FaceRecognizer fr = new FisherFaceRecognizer(10 ,double.PositiveInfinity);
+        FaceRecognizer.PredictionResult result = new FaceRecognizer.PredictionResult();
+
+
+        System.Media.SoundPlayer startSoundPlayer = new System.Media.SoundPlayer(@"C:\Users\USER\Music\alarm.wav");
+       
+
+        public FaceID()
+        {
+
+            
+            InitializeComponent();
+            new Thread(SampleFunction).Start();
+            new Thread(SampleFunction2).Start();
+            new Thread(SampleFunction3).Start();
+            this.Text = "                                                                                        FaceID  Protocol";
+
+
+            this.WindowState = FormWindowState.Maximized;
+
+
+
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+        void SampleFunction()
+        {
+
+
+        }
+
+        void SampleFunction2()
+        {
+
+
+        }
+        void SampleFunction3()
+        {
+
+
+        }
+
+
+
+
+        public void AppendTextBox(string value)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox), new object[] { value });
+                return;
+            }
+
+            textBox2.Text = value;
+
+        }
+
+        public void AppendTextBox2(string value)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox2), new object[] { value });
+                return;
+            }
+
+
+            textBox3.BackColor = Color.Red;
+
+            textBox3.Text = value;
+
+        }
+
+        public void AppendTextBox3(string value)
+        {
+
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox3), new object[] { value });
+                return;
+            }
+
+
+            textBox3.BackColor = Color.Blue;
+
+            textBox3.Text = "No Faces Detected";
+
+        }
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            videoSource.Start();
+            videoSource.NewFrame += new NewFrameEventHandler(Video_NewFrame);
+
+
+
+        }
+
+        private void Video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+
+
+
+            Image<Bgr, Byte> ColordImage = new Image<Bgr, Byte>(eventArgs.Frame);
+            Image<Gray, Byte> grayFrame = ColordImage.Convert<Gray, Byte>();
+            Image<Bgr, Byte> resultFrame = new Image<Bgr, Byte>(eventArgs.Frame);
+            Image<Bgr, Byte> save_face_frame = new Image<Bgr, Byte>(eventArgs.Frame);
+
+            haarcascade_frontalface_default = new CascadeClassifier(@"D:\OpenCV\opencv\build\etc\haarcascades\haarcascade_frontalface_default.xml");
+            var detectedFaces_default = haarcascade_frontalface_default.DetectMultiScale(grayFrame);
+
+            
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+                foreach (var detectedFace in detectedFaces_default)
+                {
+
+
+                    save_face_frame = resultFrame.GetSubRect(detectedFace);
+
+
+
+                }
+
+
+                gray_face = save_face_frame.Convert<Gray, Byte>().Resize(100, 100, Emgu.CV.CvEnum.Inter.Linear);
+                gray_face._EqualizeHist();
+
+                
+
+                    result = fr.Predict(gray_face);
+
+                
+                
+                AppendTextBox(result.Distance.ToString());
+
+            if (result.Label != 0)
+            {
+
+
+                if (result.Distance > 150)
+                {
+
+
+
+                    //textBox3.BackColor = Color.Red;
+                    AppendTextBox2(result.Label.ToString());
+
+
+
+
+
+
+
+                    pictureBox2.Image = Properties.Resources.granted;
+
+
+
+                    foreach (var detectedFace in detectedFaces_default)
+                    {
+
+                        resultFrame.Draw(detectedFace, new Bgr(0, 255, 0), 3);
+
+
+
+
+
+
+                    }
+                }
+
+                else
+
+                {
+
+
+                    pictureBox2.Image = Properties.Resources.denied;
+
+
+
+
+
+                    AppendTextBox2(result.Label.ToString());
+
+
+
+
+
+
+
+                    foreach (var detectedFace in detectedFaces_default)
+                    {
+
+                        resultFrame.Draw(detectedFace, new Bgr(0, 0, 255), 3);
+
+
+
+
+
+
+
+                    }
+
+
+
+
+                }
+
+                pictureBox1.Image = resultFrame.ToBitmap();
+
+
+
+
+
+
+
+
+
+            }
+
+
+                //  throw new NotImplementedException();
+            }
+           
+
+
+
+        
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            videoSource.SignalToStop();
+            //fr.Write(Application.StartupPath + "trainingImages.yml");
+
+        }
+
+
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            k++;
+
+            trainingImages.Add(gray_face);
+            labels.Add(k);
+            NamePersons.Add(textBox1.Text);
+
+
+
+            face = trainingImages.ToArray();
+            faceName = NamePersons.ToArray();
+            faceNum = labels.ToArray();
+
+            fr.Train(face, faceNum);
+
+
+
+            foreach (var trainingImage in trainingImages)
+            {
+
+
+                trainingImage.ToBitmap().Save(@"C:\Users\USER\Desktop\Test Faces\check2\Images\" + k + ".bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+
+
+            }
+
+
+            StreamWriter file1 =
+           new StreamWriter(@"C:\Users\USER\Desktop\Test Faces\check2\NameList\NameList.txt");
+
+            foreach (var NamePerson in NamePersons)
+            {
+
+
+                file1.WriteLine(NamePerson);
+
+
+
+
+
+            }
+
+            file1.Flush();
+            file1.Close();
+            file1.Dispose();
+
+
+
+
+
+            StreamWriter file2 =
+           new StreamWriter(@"C:\Users\USER\Desktop\Test Faces\check2\NameList\labels.txt");
+
+            foreach (var k in labels)
+            {
+
+                file2.WriteLine(k);
+
+            }
+            file2.Flush();
+            file2.Close();
+            file2.Dispose();
+
+
+
+
+
+        }
+
+
+
+
+         
+
+        private void FaceID_Load(object sender, EventArgs e)
+        {
+
+
+
+            string line;
+            System.IO.StreamReader file1 =
+                   new System.IO.StreamReader(@"C:\Users\USER\Desktop\Test Faces\check2\NameList\NameList.txt");
+
+            while ((line = file1.ReadLine()) != null)
+            {
+
+
+                NamePersons.Add(line);
+
+
+
+            }
+
+
+
+            int l;
+            System.IO.StreamReader file2 =
+                   new System.IO.StreamReader(@"C:\Users\USER\Desktop\Test Faces\check2\NameList\labels.txt");
+
+
+            while ((line = file2.ReadLine()) != null)
+            {
+
+                l = Int32.Parse(line);
+                labels.Add(l);
+                k++;
+
+
+
+
+            }
+
+            var path = @"C:\Users\USER\Desktop\Test Faces\check2\Images\";
+            string[] filePaths = Directory.GetFiles(path, "*.bmp");
+
+
+
+            foreach (var filePath in filePaths)
+            {
+
+
+
+
+                Bitmap bmp = (Bitmap)Image.FromFile(filePath);
+
+                Image<Gray, Byte> myImage = new Image<Gray, byte>(bmp);
+
+                trainingImages.Add(myImage);
+
+
+            }
+
+
+
+            file1.Close();
+            file2.Close();
+
+
+
+
+            face = trainingImages.ToArray();
+            faceName = NamePersons.ToArray();
+            faceNum = labels.ToArray();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            fr.Train(face, faceNum);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
